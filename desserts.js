6aa6bbc694e719a73abfcf0e9e15b74d80be8421
Abigael Jeptoo <jeptooabigael164@gmail.com>
2026-01-18 13:56:10 -0800

    Add dessert data array

---
+++ desserts.js
@@ -0,0 +1,31 @@
+const desserts = [
+    {id:1, 
+        title: "Chocolate Dream Delight",
+        category:"Chocolate desserts",
+        image:"/chocolate.png",
+        price: 200 , 
+        desc: "A rich and creamy chocolate dessert made with smooth custard, layered with crushed biscuits and topped with chocolate drizzle. Perfect for chocolate lovers."
+    },
+    
+    {id:2, 
+        title:"Vanilla Berry Bliss",
+        category:"Fruit & Vanilla Desserts",
+        price: 180,
+        desc:"Light vanilla custard combined with sweet strawberry jam and biscuit layers. A refreshing and fruity dessert that’s not too heavy."
+    },
+
+    {id:3,
+        title:"Caramel Crunch Cup",
+        category:"Caramel & Crunch Desserts",
+        price: 220,
+        desc:"Creamy caramel-flavoured custard layered with crunchy biscuit crumbs and finished with caramel sauce on top for a sweet, buttery taste."
+    },
+    {id:4,
+        title:"Mocha Treat Supreme",
+        category:"Coffee_Based Desserts",
+        price: 230,
+        desc:"A delicious blend of coffee and chocolate flavours, layered with soft cake or biscuits. Perfect for coffee lovers and late-night study vibes."
+    }
+
+]
+export default desserts;
