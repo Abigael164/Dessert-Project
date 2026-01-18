25638c14ad63567747d904249c385c3c547fa5df
Abigael Jeptoo <jeptooabigael164@gmail.com>
2026-01-18 14:05:18 -0800

    Add About page component

---
+++ About.jsx
@@ -0,0 +1,22 @@
+const About = () =>{
+    return(
+        <div className="about">
+            <h2>About Us</h2>
+            <p>We are passionate about creating delicious, affordable dessert cups made for 
+                everyday moments. <br/>Our desserts are freshly prepared using quality ingredients 
+                and carefully layered to deliver rich flavor in every spoonful.<br/>
+                What started as a simple idea to make sweet treats more accessible 
+                has grown into a small brand focused on taste, consistency, and value.<br/>
+                Whether you’re taking a study break, celebrating a special occasion, or looking 
+                for the perfect gift, our dessert cups are made to bring joy and satisfaction.<br/>
+                We believe good desserts don’t have to be expensive. That’s why our menu is 
+                designed with students and dessert lovers in mind—simple, tasty, and made with care.
+            </p>
+        <h4>Sweet moments start here!🍰</h4>
+
+
+        </div>
+    )
+
+}
+export default About;
